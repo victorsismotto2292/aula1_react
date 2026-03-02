05287619e3525e1bb7f647d83cc0bca5a896cfa7
= <167320276+victorsismotto2292@users.noreply.github.com>
2026-03-02 12:20:47 -0300

    Update about.js
    Colocando botão de contato

--- my-app/src/app/about.js
+++ my-app/src/app/about.js
@@ -1,5 +1,10 @@
 export default function About(){
     return(
-        <h1>Olá! Sou o About da Home.</h1>
+        <div>
+            <div>
+                <h1>Olá! Sou o About da Home.</h1>
+                <button>Botão de contato</button>
+            </div>
+        </div>
     )
 }
